61cb88295aed4309f91a9993a9e310a019722de4
geekp2p <geekp2p@gmail.com>
2025-08-24 16:21:27 +0700

    Add files via upload

---
+++ relay/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	libp2p "github.com/libp2p/go-libp2p"
+	relayv2 "github.com/libp2p/go-libp2p/p2p/protocol/circuitv2/relay"
+)
+
+func main() {
+	// อ่าน config จาก ENV
+	listen := os.Getenv("RELAY_LISTEN")
+	if listen == "" {
+		listen = "/ip4/0.0.0.0/tcp/4003"
+	}
+
+	// สร้าง host ที่ฟังที่ listen address
+	h, err := libp2p.New(libp2p.ListenAddrStrings(listen), libp2p.EnableRelay())
+	if err != nil {
+		panic(err)
+	}
+	defer h.Close()
+
+	// เปิด Circuit Relay v2
+	_, err = relayv2.New(h)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("✅ Relay PeerID: %s\n", h.ID())
+	for _, a := range h.Addrs() {
+		fmt.Printf("📡 Listening on: %s/p2p/%s\n", a, h.ID())
+	}
+
+	// รอ signal เพื่อปิด
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	<-ch
+	fmt.Println("🛑 Shutting down relay...")
+	_ = ctx
+}
