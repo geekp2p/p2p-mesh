74a0eeec423616b0da805bacae7a9932d51df117
geekp2p <geekp2p@gmail.com>
2025-08-26 22:18:37 +0700

    Add files via upload

--- run-relay.bat
+++ run-relay.bat
@@ -1,5 +1,10 @@
 @echo off
-cd /d C:\Users\Administrator\p2p-mesh
+
+REM Change to the script directory so the relay can be started from anywhere
+cd /d "%~dp0"
+
 REM Relay does not need a room, but set default to match other nodes
 set APP_ROOM=my-room
-.\bin\relay.exe
+
+REM Run the relay binary from the local bin folder
+"%~dp0\bin\relay.exe
